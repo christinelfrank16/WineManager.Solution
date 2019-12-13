8772a53e73814ec6517d7fed2dbb30b3fb0e09cd
Christine Frank <christine.braun13@gmail.com>
2019-12-13 14:22:17 -0800

    Add custom hook to determine viewport size for use instead of media queries

--- WineManager/ClientApp/src/App.js
+++ WineManager/ClientApp/src/App.js
@@ -5,11 +5,15 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/locations' component={Counter} />
-    <Route path='/favorites/:startDateIndex?' component={FetchData} />
-    <Route path='/profile' />
-  </Layout>
-);
+
+export default () => {
+
+  return(
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/locations/:locationId?' component={Counter} />
+      <Route path='/favorites/:favoriteId?' component={FetchData} />
+      <Route path='/profile' />
+    </Layout>
+  )
+};
--- WineManager/ClientApp/src/components/Home.js
+++ WineManager/ClientApp/src/components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import useBreakpoints from './hooks/ViewportSize';
 import Card from './Card';
 
 function Home(props){
+  const queries = {
+    xs: '(max-width: 320px)', //query for xs devices
+    sm: '(max-width: 720px)',
+    md: '(max-width: 1024px)'
+  }
+  const point = useBreakpoints(queries);
   return(
     <div >
 
@@ -10,7 +17,7 @@ function Home(props){
 
         <Card title='Profile' links={[{ name: 'Go', link: '/profile' }]} />
         
-        <Card title='Locations' links={[{name: 'test location 1', link: '/location/1'}]}/>
+        <Card title='Locations' links={[{name: 'test location 1', link: '/locations/1'}]}/>
 
     </div>
   )
