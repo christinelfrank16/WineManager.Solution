ad3f5d16368ffdb2476cd90251db5f042e24e39f
Student <example@example.com>
2019-12-16 09:09:17 -0800

    Add Swagger to startup and csproj

--- WineManager/Startup.cs
+++ WineManager/Startup.cs
@@ -36,6 +36,9 @@ namespace WineManager.Solution
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            // Register the Swagger services
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +54,11 @@ namespace WineManager.Solution
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
 
             // app.UseHttpsRedirection();
             app.UseStaticFiles();
