23f429183d1c8159c0a09b8df22efa41e7252d16
Student <example@example.com>
2019-12-17 11:49:47 -0800

    Style search bar and add switch input

--- WineManager/ClientApp/src/components/AddWine.js
+++ WineManager/ClientApp/src/components/AddWine.js
@@ -2,22 +2,42 @@ import React from 'react';
 import { Form, Input, Button } from 'reactstrap';
 
 function AddWine(){
-    const shortInputStyle = {
-        width: '6vw'
-    }
-    const header = {
-        textAlign: 'center'
-    }
     const pageWidth = {
         width: 'inherit'
     }
+    const alignCenter = {
+        textAlign: 'center'
+    }
+    const shortInputStyle = {
+        width: '7vw'
+    }
+    const longInputStyle = {
+        width: '9vw'
+    }
+    const switchStyle = {
+        marginLeft: '2vw'
+    }
+
     return(
         <div style={pageWidth}>
-            <h3 style={header}>Add Wine</h3>
+            <h3 style={alignCenter}>Add Wine</h3>
             <Form inline>
-                <Input type="text" placeholder="Search" className="mr-sm-2" />
-                <Input type="number" placeholder="Vintage" className="mr-sm-2" style={shortInputStyle} />
-                <Button variant="outline-success">Search</Button>
+                <div>
+                    <Input type="text" placeholder="Search" className="mr-sm-2" style={longInputStyle}/>
+                    <Input type="number" placeholder="Vintage" className="mr-sm-2" style={shortInputStyle} />
+                    <Button variant="outline-success">Search</Button>
+                </div>
+                <div style={switchStyle} className='custom-control custom-switch'>
+                    <input
+                    type='checkbox'
+                    className='custom-control-input'
+                    id='searchOutsideCollection'
+                    readOnly
+                    />
+                    <label className='custom-control-label' htmlFor='searchOutsideCollection'>
+                    Search outside collection
+                    </label>
+                </div>
             </Form>
         </div>
     )
