d0a040f264561c9a27617e74009d6795fea96a5e
Student <example@example.com>
2019-12-17 13:45:45 -0800

    Start creating list table for list view

--- WineManager/ClientApp/src/components/ListData.js
+++ WineManager/ClientApp/src/components/ListData.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 
-function ListData(){
+function ListData(props){
+    const pageWidth = {
+        width: 'inherit',
+        height: '95%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start'
+    }
+    const alignCenter = {
+        textAlign: 'center'
+    }
+    function generateBodyRows(){
+
+    }
     return(
-        <div>
-            ListData
+        <div style={pageWidth}>
+            <h3 style={alignCenter}>Add Wine</h3>
+            <Table hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Slot</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {generateBodyRows()}
+                </tbody>
+            </Table>
         </div>
     )
 }
