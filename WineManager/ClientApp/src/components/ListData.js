50c4559d517bfc4b9d15be87f6f5b32e811a994d
Student <example@example.com>
2020-01-02 12:25:58 -0800

    Allow wine list to be sorted on selection of header value

--- WineManager/ClientApp/src/components/ListData.js
+++ WineManager/ClientApp/src/components/ListData.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'reactstrap';
 
 function ListData(props){
+    const [orderedWineList, updateList] = useState(props.slots);
+    const [selectedTableHeader, updateHeaderValue] = useState(null);
     const pageWidth = {
         marginTop: '8px',
         width: 'inherit',
@@ -17,7 +19,7 @@ function ListData(props){
         backgroundColor: 'lightblue'
     }
     function generateBodyRows(){
-        return props.slots.map((slot) => {
+        return orderedWineList.map((slot) => {
             const { slotId, wineItem, position } = slot;
             const { name, description, style } = wineItem;
             const { value } = position;
@@ -34,15 +36,57 @@ function ListData(props){
         const location = event.target.closest("tr").id;
         props.updateSelectedSlot(location);
     }
+    function updateHeader(value){
+        if(selectedTableHeader === value){
+            updateHeaderValue(null);
+            updateList(props.slots);
+        } else {
+            updateHeaderValue(value);
+            sortListByProp(value);
+        }
+    }
+
+    function sortListByProp(sortingProp){
+        sortingProp = sortingProp === 'Type' ? 'style' : sortingProp;
+        sortingProp = sortingProp === 'Slot' ? 'value' : sortingProp;
+        sortingProp = sortingProp === 'Name' ? 'name' : sortingProp;
+
+        if(sortingProp === 'value'){
+            orderedWineList.sort(function(itemA, itemB){
+                let valueA = itemA.position.value.substring(0, itemA.position.value.indexOf("-"));
+                let valueB = itemB.position.value.substring(0, itemA.position.value.indexOf("-"));
+                if(valueA < valueB){
+                    return -1;
+                }
+                if(valueA > valueB){
+                    return 1;
+                }
+                return 0;
+            });
+        } else {
+            orderedWineList.sort(function(itemA, itemB){
+                let valueA = itemA.wineItem[sortingProp];
+                let valueB = itemB.wineItem[sortingProp];
+                if(valueA < valueB){
+                    return -1;
+                }
+                if(valueA > valueB){
+                    return 1;
+                }
+                return 0;
+            });
+        }
+    }
+
     return(
         <div style={pageWidth}>
-            <h3 style={alignCenter}>Data List</h3>
+            <h3 style={alignCenter}>Wine List</h3>
             <Table hover>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Slot</th>
+                        <th style={selectedTableHeader === 'Name' ? highlightRow : null } onClick={() => updateHeader('Name')}>Name</th>
+                        <th style={selectedTableHeader === 'Type' ? highlightRow : null } onClick={() =>  updateHeader('Type')}>Type</th>
+                        <th style={selectedTableHeader === 'Slot' ? highlightRow : null } onClick={() =>  updateHeader('Slot')}>Slot</th>
                     </tr>
                 </thead>
                 <tbody>
